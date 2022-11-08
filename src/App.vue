320f4e968907170ec43da2f01a791e01315fbc83
juiling <2min0903@gmail.com>
2022-11-08 22:54:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <div
+    v-for="item in data.newsdata.data"
+    :key="item.seq">
+    <p>{{item.旅宿名稱}}</p>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+import{reactive,onMounted} from 'vue';
+
+//高雄市一般旅館資料
+const url = 'https://api.kcg.gov.tw/api/service/Get/8ed53368-e292-4e2a-80a7-434cf497220c';
+
+
+// const App = {
+//   setup() {
+    
+     const data = reactive({
+          newsdata:'',
+        })
+
+    onMounted(() => {
+
+      axios.get(url)
+      .then((res)=>{
+        console.log(res.data)
+        data.newsdata = res.data
+    })
+  });
+    // return {data};
+  // },
+// }
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/componentsA.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <p>單向綁定</p>
+    {{msg}}
+</div>
+    
+<div>
+    <p>雙向綁定</p>
+    <input type = "text" v-model = "text"/>
+    <p>{{text}}</p>
+</div>
+
+</template>
+    
+
+<script setup>
+import { ref } from 'vue';
+const msg = 'temp Vue'
+const text = ref("Vue3")
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
